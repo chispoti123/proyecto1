7d9569c69422f94b54e8f610ac7258cc5661125a
garri <201804713j@utea.edu.pe>
2022-05-04 23:11:58 -0500

    Realizando la conexion

--- index.php
+++ index.php
@@ -1,40 +1,59 @@
+<?php 
+
+include 'config.php';
+
+session_start();
+
+error_reporting(0);
+
+if (isset($_SESSION['username'])) {
+    header("Location: welcome.php");
+}
+
+if (isset($_POST['submit'])) {
+	$email = $_POST['email'];
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $row['username'];
+		header("Location: welcome.php");
+	} else {
+		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+	}
+}
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<title>Login Form - Pure Coding</title>
 </head>
-
 <body>
-    <div id="wrapper">
-
-        <form name="login-form" class="login-form" action="" method="post">
-        
-            <div class="header">
-            <h1>Login</h1>
-            <span>Complete el formulario a continuación para iniciar sesión</span>
-            </div>
-        
-            <div class="content">
-            <input name="username" type="text" class="input username" placeholder="Username" />
-            <div class="user-icon"></div>
-            <input name="password" type="password" class="input password" placeholder="Password" />
-            <div class="pass-icon"></div>		
-            </div>
-    
-            <div class="footer">
-            <input type="submit" name="submit" value="Login" class="button" />
-            <a href="index2.html">Ir registro</a>
-            </div>
-        
-        </form>
-    
-    </div>
-    <div class="gradient"></div>
+	<div class="container">
+		<form action="" method="POST" class="login-email">
+			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+			<div class="input-group">
+				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+			</div>
+			<div class="input-group">
+				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+			</div>
+			<div class="input-group">
+				<button name="submit" class="btn">Login</button>
+			</div>
+			<p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p>
+		</form>
+	</div>
 </body>
 </html>
